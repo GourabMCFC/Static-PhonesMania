4d75670d4b58b3835db06148971bc40bd3e6dd7c
GourabMCFC <gourab.mukherjee.fiem.cse20@teamfuture.in>
2023-10-26 21:29:44 +0530

    Some Content Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,13 @@
 module.exports = {
   content: ["index.html"],
   theme: {
+    container: {
+      center: true
+    },
     extend: {
-      fontFamily:{
-        dance:['Dancing Script'],
-        satisfy:['Satisfy']
+      fontFamily: {
+        dance: ['Dancing Script'],
+        satisfy: ['Satisfy']
       }
     },
   },
